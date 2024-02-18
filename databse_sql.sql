2c9e948547d9307f8b2f48ad46b9fca1ff016692
sohag47 <minhazul.islamcse@gmail.com>
2024-02-18 23:14:57 +0600

    add get row sql

--- databse_sql.sql
+++ databse_sql.sql
@@ -1,3 +1,4 @@
+-- Active: 1708267180025@@127.0.0.1@3306@learning_db
 -- ! Database SQL
 
 -- create database
@@ -5,6 +6,10 @@ CREATE DATABASE learning_db;
 
 -- delete database
 DROP DATABASE learning_db;
+
+-- show all DATABASES
+SHOW DATABASES;
+
 
 -- ! Table SQL
 
@@ -24,7 +29,7 @@ DROP TABLE users;
 
 -- create column
 ALTER TABLE users 
-ADD email varchar(255) NOT NULL;
+ADD age INT NULL;
 
 -- update column column name
 ALTER TABLE users 
@@ -36,4 +41,52 @@ CHANGE COLUMN name name varchar(255) NULL DEFAULT NULL;
 
 -- delete column
 ALTER TABLE users 
-DROP COLUMN email;
+DROP COLUMN name;
+
+
+--! Row Execute sql
+-- create row data users table
+INSERT INTO users (id, fname) VALUES (2, "Sharier Rahman");
+
+-- show all row data users table
+SELECT * FROM users;
+
+-- update row data users table
+UPDATE users SET age = 5 WHERE id = 2;
+
+--? delete row data users table
+-- delete specific row
+DELETE FROM users WHERE id = 3;
+
+-- delete all row
+DELETE FROM users;
+
+--* Read row with ORDER BY(ASC/DESC) 
+SELECT * FROM users ORDER BY id DESC;
+SELECT * FROM users ORDER BY id ASC;
+
+--* use NULL value
+SELECT * FROM users WHERE lname IS NULL;
+SELECT * FROM users WHERE lname IS NOT NULL;
+
+--* SELECT TOP Clause
+SELECT * FROM users LIMIT 1;
+SELECT * FROM users WHERE id=2 LIMIT 3;
+
+--* SELECT MIN() and MAX()
+SELECT MIN(age) as SmallestAge FROM users;
+SELECT MAX(age) as HeightAge FROM users;
+
+--* count row 
+SELECT COUNT(age) AS count_users FROM users;
+-- Ignore Duplicates
+SELECT COUNT(DISTINCT age) AS count_users FROM users WHERE age > 5;
+
+--* sum row 
+SELECT SUM(age * 5) AS total_ages FROM users;
+
+--* LIKE Operator
+SELECT * FROM users WHERE lname LIKE 's__ag%';
+
+--* IN Operator
+-- SELECT * FROM users WHERE IN age (5);
